e83e6db3895ee6a86440e0ad21ae187ba7248d49
William Hleucka <william.hleucka@gmail.com>
2025-02-03 03:06:57 +0000

    fix: update db container

--- app/Config/db.php
+++ app/Config/db.php
@@ -12,6 +12,5 @@ return [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
     PDO::ATTR_PERSISTENT         => false,
-    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4",
     ]
 ];
