022fa33988e771a53e9751d20a80ce4061b6a26b
William Hleucka <william.hleucka@gmail.com>
2025-05-14 11:55:38 -0600

    wip: setup page

--- app/Http/Controllers/Setup/SetupController.php
+++ app/Http/Controllers/Setup/SetupController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Setup;
 
 use Echo\Framework\Http\Controller;
-use Echo\Framework\Routing\Route\Get;
+use Echo\Framework\Routing\Route\{Get, Post};
 
 class SetupController extends Controller
 {
@@ -11,5 +11,27 @@ class SetupController extends Controller
     public function index(): string
     {
         return $this->render("setup/index.html.twig");
+    }
+
+    #[Post("/setup", "setup.post")] 
+    public function post(): string
+    {
+        $valid = $this->validate([
+            "app_name" => [],
+            "app_url" => [],
+            "app_debug" => [],
+            "db_name" => [],
+            "db_username" => [],
+            "db_password" => [],
+            "db_host" => [],
+            "db_port" => [],
+            "db_charset" => [],
+        ]);
+
+        if ($valid) {
+            dd("WIP");
+        }
+        
+        return $this->index();
     }
 }
