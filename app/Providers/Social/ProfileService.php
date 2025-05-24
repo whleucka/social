52dd998eb314faa64d75a10d1b28e3c27a29e421
William Hleucka <william.hleucka@gmail.com>
2025-05-23 21:23:31 -0600

    add: ProfileService

---
+++ app/Providers/Social/ProfileService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Providers\Social;
+
+use App\Models\User;
+
+class ProfileService
+{
+    public function getUserByUsername(string $username)
+    {
+        $user = User::where("username", $username)->get();
+        if ($user) {
+            return [
+                "gravatar" => $user->gravatarUrl(),
+                "name" => $user->first_name . ' ' . $user->surname,
+                "username" => $user->username,
+            ];
+        }
+    }
+}
