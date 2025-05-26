38ff84b80ed621840a52bc0ae179790d1e848fce
William Hleucka <william.hleucka@gmail.com>
2025-05-26 11:31:23 -0600

    feat: linkify filter twig

--- src/Framework/View/TwigExtension.php
+++ src/Framework/View/TwigExtension.php
@@ -3,6 +3,7 @@
 namespace Echo\Framework\View;
 
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class TwigExtension extends AbstractExtension
@@ -12,7 +13,14 @@ class TwigExtension extends AbstractExtension
         return [
             new TwigFunction("csrf", [$this, "csrf"]),
             new TwigFunction("uri", [$this, "uri"]),
-            new TwigFunction("old", [$this, "old"])
+            new TwigFunction("old", [$this, "old"]),
+        ];
+    }
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter("linkify", [$this, "linkify"]),
         ];
     }
 
@@ -30,5 +38,23 @@ class TwigExtension extends AbstractExtension
     public function old(string $name, mixed $default = null)
     {
         return request()->request->get($name) ?? $default;
+    }
+
+    function linkify(string $text): string {
+        // Escape first to prevent XSS
+        $escaped = htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
+
+        // Convert URLs to <a> links
+        $linked = preg_replace_callback(
+            '/(https?:\/\/[^\s<]+)/i',
+            function ($matches) {
+                $url = $matches[1];
+                return "<a href=\"$url\" target=\"_blank\" rel=\"noopener noreferrer\">$url</a>";
+            },
+            $escaped
+        );
+
+        // Convert newlines to <br>
+        return nl2br($linked, false); // false: don't use XHTML-style <br />
     }
 }
