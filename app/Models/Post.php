4a4fe081c5296e4aa9f386f86b3068b47f7a0469
William Hleucka <william.hleucka@gmail.com>
2025-05-27 11:57:52 -0600

    fix: twitter-link ts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,9 +17,21 @@ class Post extends Model
         return $this->parent_id ? Post::find($this->parent_id) : null;
     }
 
+    public function shortTimeDiff(Carbon $time, ?Carbon $now = null): string {
+        $now = $now ?? Carbon::now();
+        $diff = $time->diff($now);
+
+        if ($diff->y > 0) return $diff->y . 'y';
+        if ($diff->m > 0) return $diff->m . 'mo';
+        if ($diff->d > 0) return $diff->d . 'd';
+        if ($diff->h > 0) return $diff->h . 'h';
+        if ($diff->i > 0) return $diff->i . 'm';
+        return $diff->s . 's';
+    }
+
     public function ago()
     {
-        return Carbon::parse($this->created_at)->diffForHumans();
+        return $this->shortTimeDiff(Carbon::parse($this->created_at));
     }
 
     public function isLiked(int $user_id)
