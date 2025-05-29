90cbdb7545721cd15304f96485a7cd2a0e191d77
William Hleucka <william.hleucka@gmail.com>
2025-05-29 13:55:53 -0600

    fix: version bump

--- config/app.php
+++ config/app.php
@@ -4,5 +4,5 @@ return [
     "debug" => env("APP_DEBUG", true),
     "url" => env("APP_URL", "http://0.0.0.0:8000"),
     "name" => env("APP_NAME", "Echo"),
-    "version" => "0.0.1",
+    "version" => "0.3.0",
 ];
