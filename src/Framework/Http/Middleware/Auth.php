4d60c850ebc12ca5be72c5d9eae7457680825ddd
William Hleucka <william.hleucka@gmail.com>
2025-06-03 17:20:01 +0000

    fix: redirect sign in

--- src/Framework/Http/Middleware/Auth.php
+++ src/Framework/Http/Middleware/Auth.php
@@ -22,6 +22,7 @@ class Auth implements Middleware
 
         if (in_array('auth', $middleware) && !$user) {
             $res = new HttpResponse('<script>window.location.href="/sign-in";</script>', 200);
+            $res->setHeader("HX-Retarget", "body");
             session()->set("auth_redirect", $_SERVER["REQUEST_URI"]);
             Flash::add("warning", "Please sign in to view this page.");
             return $res;
